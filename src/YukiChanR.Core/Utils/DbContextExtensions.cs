207c275e5b495aae4d2e3629e758a5415105deb2
bsdayo <dev@sorabs.cc>
2023-05-28 11:49:24 +0800

    feat(arcaea): implement a.set

---
+++ src/YukiChanR.Core/Utils/DbContextExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace YukiChanR.Core.Utils;
+
+public static class DbContextExtensions
+{
+    public static async Task<T> FirstOrAddAsync<T>(this DbSet<T> set,
+        Expression<Func<T, bool>> predicate) where T : class, new()
+    {
+        var entity = await set.FirstOrDefaultAsync(predicate);
+
+        if (entity is null)
+        {
+            entity = new T();
+            set.Entry(entity).State = EntityState.Added;
+        }
+
+        return entity;
+    }
+
+    public static async Task<T> FirstOrAddAsync<T>(this DbSet<T> set,
+        Expression<Func<T, bool>> predicate, Func<T> entityFactory) where T : class
+    {
+        ArgumentNullException.ThrowIfNull(entityFactory);
+
+        var entity = await set.FirstOrDefaultAsync(predicate);
+
+        if (entity is null)
+        {
+            entity = entityFactory();
+            set.Entry(entity).State = EntityState.Added;
+        }
+
+        return entity;
+    }
+}
--- src/YukiChanR.Plugins.Arcaea/Commands/Set.cs
+++ src/YukiChanR.Plugins.Arcaea/Commands/Set.cs
@@ -1,55 +1,50 @@
-﻿// using Flandre.Core.Messaging;
-// using Flandre.Framework.Attributes;
+﻿using Flandre.Core.Messaging;
+using Flandre.Framework.Attributes;
+using Flandre.Framework.Common;
+using YukiChanR.Core.Utils;
+using YukiChanR.Plugins.Arcaea.Entities;
 
-// // ReSharper disable CheckNamespace
+// ReSharper disable CheckNamespace
 
-// namespace YukiChanR.Plugins.Arcaea;
+namespace YukiChanR.Plugins.Arcaea;
 
-// public partial class ArcaeaPlugin
-// {
-//     [Command("a.set")]
-//     public async Task<MessageContent> OnSet(MessageContext ctx, string preferences)
-//     {
-//         var prefStrList = preferences
-//             .Replace('，', ',')
-//             .Replace('；', ',')
-//             .Replace(';', ',')
-//             .Split(',');
-        
-//         var prefResp = await _yukiClient.Arcaea.GetPreferences(ctx.Platform, ctx.UserId);
-//         if (!prefResp.Ok) return ctx.ReplyServerError(prefResp);
-        
-//         var pref = prefResp.Data.Preferences;
-        
-//         bool ParseBoolSet(string[] set)
-//             => set.Length <= 1 || !set[1].Equals("false", StringComparison.OrdinalIgnoreCase);
-        
-//         foreach (var prefStr in prefStrList)
-//         {
-//             var set = prefStr.Split('=');
-//             switch (set[0].ToLower().Replace("_", ""))
-//             {
-//                 case "dark":
-//                     pref.Dark = ParseBoolSet(set);
-//                     break;
-        
-//                 case "nya":
-//                     pref.Nya = ParseBoolSet(set);
-//                     break;
-        
-//                 case "singledynamicbg":
-//                     pref.SingleDynamicBackground = ParseBoolSet(set);
-//                     break;
-        
-//                 case "b30showgrade":
-//                     pref.Best30ShowGrade = ParseBoolSet(set);
-//                     break;
-//             }
-//         }
-        
-//         var updateResp = await _yukiClient.Arcaea.UpdatePreferences(ctx.Platform, ctx.UserId,
-//             new ArcaeaPreferencesRequest { Preferences = pref });
-//         if (!updateResp.Ok) ctx.ReplyServerError(updateResp);
-//         return ctx.Reply("已成功为您更新偏好信息。");
-//     }
-// }
+public partial class ArcaeaPlugin
+{
+    [Command("a.set")]
+    public async Task<MessageContent> OnSet(CommandContext ctx, params string[] preferences)
+    {
+        var pref = await _database.Preferences
+            .FirstOrAddAsync(
+                p => p.Platform == ctx.Platform && p.UserId == ctx.UserId,
+                () => new ArcaeaUserPreferences { Platform = ctx.Platform, UserId = ctx.UserId });
+
+        bool ParseBoolSet(string[] set)
+            => set.Length <= 1 || !set[1].Equals("false", StringComparison.OrdinalIgnoreCase);
+
+        foreach (var prefStr in preferences)
+        {
+            var set = prefStr.Split('=');
+            switch (set[0].ToLower().Replace("_", ""))
+            {
+                case "dark":
+                    pref.Dark = ParseBoolSet(set);
+                    break;
+
+                case "nya":
+                    pref.Nya = ParseBoolSet(set);
+                    break;
+
+                case "singledynamicbg":
+                    pref.SingleDynamicBackground = ParseBoolSet(set);
+                    break;
+
+                case "b30showgrade":
+                    pref.Best30ShowGrade = ParseBoolSet(set);
+                    break;
+            }
+        }
+
+        await _database.SaveChangesAsync();
+        return ctx.Reply("已成功为您更新偏好信息。");
+    }
+}
